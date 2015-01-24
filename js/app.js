e6749069c667cda866da73fb651354826e47bc6a
Tri Nguyen <tri@tridnguyen.com>
2015-01-24 01:34:17 -0500

    start with ampersand

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 'use strict';
 
+var MainView = require('./views/main');
+
+var main = new MainView();
+main.render();
---
+++ js/views/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var View = require('ampersand-view');
+var template = require('../../templates/main.hbs');
+
+module.exports = View.extend({
+	template: template
+});
