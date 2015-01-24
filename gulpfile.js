4b3fd006aac2e00a4a3f1bdad10a7eb732a0c13c
Tri Nguyen <tri@tridnguyen.com>
2015-01-24 01:06:00 -0500

    initial setup

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util');
+
+var jshint = require('gulp-jshint');
+gulp.task('jshint', function () {
+	return gulp.src('app/scripts/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(require('jshint-stylish')));
+});
+
+var watching = false;
+gulp.task('enable-watch-mode', function () { watching = true });
+
+var browserify = require('browserify');
+var watchify = require('watchify');
+var xtend = require('xtend');
+var source = require('vinyl-source-stream');
+var hbsfy = require('hbsfy');
+gulp.task('scripts', function () {
+	var opts = {
+		entries: './js/app.js',
+		debug: (gutil.env.type === 'development')
+	}
+	if (watching) {
+		 opts = xtend(opts, watchify.args);
+	}
+	var bundler = browserify(opts);
+	if (watching) {
+		bundler = watchify(bundler);
+	}
+	// optionally transform
+	bundler.transform(hbsfy);
+
+	bundler.on('update', function (ids) {
+		gutil.log('File(s) changed: ' + gutil.colors.cyan(ids));
+		gutil.log('Rebunlding...');
+		rebundle();
+	});
+
+	function rebundle() {
+		return bundler
+			.bundle()
+			.on('error', function (e) {
+				 gutil.log(gutil.colors.red('Browserify ' + e));
+			})
+			.pipe(source('main.js'))
+			.pipe(gulp.dest('dist'));
+	}
+	return rebundle();
+});
+
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+gulp.task('styles', function () {
+    return gulp.src('scss/**/*.scss')
+		.pipe(plumber({
+			errorHandler: function (err) {
+				gutil.log(gutil.colors.red('Styles error:\n' + err.message));
+				// emit `end` event so the stream can resume https://github.com/dlmanning/gulp-sass/issues/101
+				if (this.emit) {
+					this.emit('end');
+				}
+			}
+		}))
+		.pipe(sass())
+		.pipe(prefix())
+		.pipe(csso())
+		.pipe(gulp.dest('dist'));
+});
+
+var connect = require('gulp-connect');
+gulp.task('connect', function () {
+	return connect.server({
+		port: 4005
+	});
+});
+
+gulp.task('watch', ['enable-watch-mode', 'scripts', 'styles', 'connect'], function () {
+	gulp.watch('scss/**/*.scss', ['styles']);
+});
