783d2df4360309c4ef4ccaa30764eee45cbc4b07
Tri Nguyen <tri@tridnguyen.com>
2015-02-16 21:44:49 -0500

    add transformicons

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var MainView = require('./views/main');
+var transformicons = require('./lib/transformicons');
 
 var main = new MainView({
 	el: document.querySelector('.main')
 });
 main.render();
+
+transformicons.add('.tcon');
