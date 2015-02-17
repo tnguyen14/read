efa34daa75dda3d9435f67a90a1b29242350a6bb
Tri Nguyen <tri@tridnguyen.com>
2015-02-16 21:45:18 -0500

    add loader to articles

--- js/views/articles.js
+++ js/views/articles.js
@@ -2,14 +2,18 @@
 
 var View = require('ampersand-view');
 var ArticleView = require('./article');
+var Loader = require('./loader');
 var template = require('../../templates/articles.hbs');
 
 module.exports = View.extend({
 	template: template,
 	render: function () {
 		this.renderWithTemplate();
+		this.loader = new Loader().render();
+		this.el.appendChild(this.loader.el);
 		this.collection.fetch().then(function () {
 			this.renderCollection(this.collection, ArticleView);
+			this.loader.remove();
 		}.bind(this));
 	}
 });
