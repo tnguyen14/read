7bc0944686cffdce7873a5ffe1206c4ab1f05e33
Tri Nguyen <tri@tridnguyen.com>
2018-06-02 23:11:22 -0400

    update travis build steps

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_539d231ab808_key -iv $encrypted_539d231ab808_iv -in .travis/muffin.enc -out .travis/muffin -d
+openssl aes-256-cbc -K $encrypted_2ef622fe4472_key -iv $encrypted_2ef622fe4472_iv -in .travis/muffin.enc -out .travis/muffin -d
 chmod 600 .travis/muffin
 eval "$(ssh-agent -s)" # start the ssh agent
 ssh-add .travis/muffin
