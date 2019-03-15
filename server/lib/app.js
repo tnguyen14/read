75f8639dfa83c7b7936756cb63ae9da7ead0bac3
Tri Nguyen <tri@tridnguyen.com>
2019-03-15 15:08:55 -0400

    use jsonBodyParser instead of bodyParser

--- server/lib/app.js
+++ server/lib/app.js
@@ -17,7 +17,7 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser({
 	mapParams: true
 }));
-server.use(restify.plugins.bodyParser({
+server.use(restify.plugins.jsonBodyParser({
 	mapParmas: true
 }));
 
