635fe1ac990637927f3ee62ff2ee4040d6e2aada
Tri Nguyen <tri@tridnguyen.com>
2020-04-08 00:54:54 -0400

    Fill out title and description field

--- client/Form.svelte
+++ client/Form.svelte
@@ -1,12 +1,20 @@
 <script>
-  let url;
+  let url, title, description;
   const baseUrl = 'https://read.cloud.tridnguyen.com';
+  let isRetrieving = false;
   async function extract() {
-    const resp = await fetch(`${baseUrl}/extract?url=${encodeURIComponent(url)}`)
-      .then(r => r.json())
-    console.log(resp);
+    if (isRetrieving) {
+      console.log('already being retrieved');
+      return;
+    }
+    isRetrieving = true;
+    title = description = 'Retrieving...';
+   ({title, description}= await fetch(`${baseUrl}/extract?url=${encodeURIComponent(url)}`)
+      .then(r => r.json()))
+    isRetrieving = false;
   }
 </script>
+
 <form class="clearfix">
   <h2 class="title">Save an article</h2>
   <div class="field">
@@ -15,6 +23,14 @@
       placeholder="https://coolstuff.com"
       id="link" name="link" bind:value={url}
 		on:change={extract}/>
+  </div>
+  <div class="field {title ? '' : 'inactive'}">
+    <label for="title">Title</label>
+    <input name="title" type="text" id="title" bind:value={title}/>
+  </div>
+  <div class="field {description ? '' : 'inactive'}">
+    <label for="description">Description</label>
+    <textarea name="description" id="description" bind:value={description}></textarea>
   </div>
   <button type="submit">Save</button>
 </form>
@@ -36,12 +52,12 @@
     display: block;
     margin: 2em 0;
   }
-  .field label {
+  label {
     display: inline-block;
     width: 25%;
   }
-  .field input,
-  .field textarea {
+  input,
+  textarea {
     padding: 1em;
     vertical-align: middle;
     width: 74%; /* not sure why 75% push the text box down */
