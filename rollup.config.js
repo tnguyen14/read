bda23bb41fee9d2c7a008dc6faba9258b2cce872
Tri Nguyen <tri@tridnguyen.com>
2020-04-07 22:15:18 -0400

    install svelte and rollup

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'client/index.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'dist/index.js'
+  },
+  plugins: [
+    svelte({
+      dev: !production,
+      css: css => {
+        css.write('dist/style.css')
+      }
+    }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
+
+    production && terser()
+  ]
+}
